0855711f2d6bbef87888c54427e5243d7132b358
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-05-06 14:31:21 -0400

    Update SetupLinuxVM.sh

--- SetupLinuxVM.sh
+++ SetupLinuxVM.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+# check to ensure you are running as SUDO
+if [ "$EUID" -ne 0 ]
+  then echo "WARNING: This needs to be run as SUDO!"
+  exit
+fi
 clear
 echo This script will configure a basic Ubuntu server to try and fit the role it will play within TAB or a client ...
 echo Please use the following menu to set the server parameters. Take a checkpoint before running this. Hit any key to begin ...
 read -rsn1
+# Create folder structure
+mkdir /etc/tab
+mkdir /etc/tab/conf
+mkdir /etc/tab/scripts
+mkdir /etc/tab/logs
+# Grab the baseline config file
+wget -O /etc/tab/conf/default.conf https://raw.githubusercontent.com/JustinTDCT/TAB-Production/refs/heads/main/default.conf
+
+
