81d81a5b90f7be71a1d73546be816a73d65b3c5b
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-05-07 09:12:57 -0400

    Create BounceSC.sh

---
+++ BounceSC.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Stopping service ..."
+service connectwisecontrol-24a22b9fc261d141 stop 2> /dev/null
+service connectwisecontrol-24a22b9fc261d141 status
+echo "Starting service ..."
+service connectwisecontrol-24a22b9fc261d141 start 2> /dev/null
+service connectwisecontrol-24a22b9fc261d141 status
