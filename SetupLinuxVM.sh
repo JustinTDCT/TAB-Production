63d87d6421ecc38aa4c0274a296c45b2faed6604
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-05-08 13:09:16 -0400

    Update SetupLinuxVM.sh

--- SetupLinuxVM.sh
+++ SetupLinuxVM.sh
@@ -148,31 +148,32 @@ install_docker () {
 }
 
 set_ip () {
-  echo "Creating a backup of the current profile ..."
-  # Creates a backup
-  cp /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.bk_`date +%Y%m%d%H%M`
-  rm /etc/netplan/00-installer-config.yaml
-  # Changes dhcp from 'yes' to 'no'
-  echo "Disabling DHCP"
-  sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/00-installer-config.yaml
-  # Retrieves the NIC information
-  echo "."
-  echo "Listing current IPs, take note of the interface for the NIC (should be eth0) ..."
-  ip address
-  echo "."
-  nic=`ip address | awk 'NR==7{print $2}'`
-  echo "Working with" $nic
-  # Ask for input on network configuration
-  echo "Intentional delay here for 20 seconds ... the login likes to blank the line you're on ... if this still happens you should be entering 3 things:"
-  echo "VM IP in CIDR form (IE 192.168.1.100/24), the gateway, then a comma seperated DNS IP list"
-  read -p "Enter the static IP of the server in CIDR notation: " staticip 
-  read -p "Enter the IP of your gateway: " gatewayip
-  read -p "Enter the IP of preferred nameservers (seperated by a coma if more than one): " nameserversip
-  echo
-  echo "Ready to apply new IP - NOTE: If you are SSH'd in your connection will drop and you will need to run the script again (wait about 2m), hit any key to begin ..."
-  read -s
-  # creates the new YAML file for the IP
-  cat > /etc/netplan/00-installer-config.yaml <<EOF
+  if [ $set_ip == "yes" ] ; then
+    echo
+    echo "Creating a backup of the current profile ..."
+    # Creates a backup
+    find -type f | xargs -I {} mv {} {}.bk_`date +%Y%m%d%H%M`
+    # Changes dhcp from 'yes' to 'no'
+    echo "Disabling DHCP"
+    sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/00-installer-config.yaml
+    # Retrieves the NIC information
+    echo
+    echo "Listing current IPs, take note of the interface for the NIC (should be eth0) ..."
+    ip address
+    echo
+    nic=`ip address | awk 'NR==7{print $2}'`
+    echo "Working with" $nic
+    # Ask for input on network configuration
+    echo "Intentional delay here for 20 seconds ... the login likes to blank the line you're on ... if this still happens you should be entering 3 things:"
+    echo "VM IP in CIDR form (IE 192.168.1.100/24), the gateway, then a comma seperated DNS IP list"
+    read -p "Enter the static IP of the server in CIDR notation: " staticip 
+    read -p "Enter the IP of your gateway: " gatewayip
+    read -p "Enter the IP of preferred nameservers (seperated by a coma if more than one): " nameserversip
+    echo
+    echo "Ready to apply new IP - NOTE: If you are SSH'd in your connection will drop and you will need to run the script again (wait about 2m), hit any key to begin ..."
+    read -s
+    # creates the new YAML file for the IP
+    cat > /etc/netplan/00-installer-config.yaml <<EOF
 network:
   ethernets:
     $nic
@@ -185,17 +186,21 @@ network:
         via: $gatewayip
   version: 2
 EOF
-  # sets permissions on the new file
-  chown root:root /etc/netplan/00-installer-config.yaml
-  chmod 700 /etc/netplan/00-installer-config.yaml
-  # applies the new file
-  sudo netplan apply
-  echo "Pausing for 10sec then listing interfaces ..."
-  read -t10
-  # output the IP
-  ip address
-  set_ip="done"
-  save_settings
+    # sets permissions on the new file
+    chown root:root /etc/netplan/00-installer-config.yaml
+    chmod 700 /etc/netplan/00-installer-config.yaml
+    # applies the new file
+    sudo netplan apply
+    echo "Pausing for 10sec then listing interfaces ..."
+    read -t10
+    # output the IP
+    ip address
+    set_ip="done"
+    save_settings
+  else
+    echo
+    echo "Config shows the IP was already set ..."
+  fi
 }
 
 do_install () {
