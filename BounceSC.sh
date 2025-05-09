1a20d104dcd0ccdb0d98cea8dda5614d4ce94317
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-05-09 10:44:40 -0400

    Update BounceSC.sh

--- BounceSC.sh
+++ BounceSC.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# READY FOR TESTING IN PRODUCTION
 echo "Stopping service ..."
 service connectwisecontrol-24a22b9fc261d141 stop 2> /dev/null
 service connectwisecontrol-24a22b9fc261d141 status
