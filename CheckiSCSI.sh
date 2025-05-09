d3ae39852a8047b33f55a655f428606f5e3ad26d
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-05-09 06:46:59 -0400

    Update CheckiSCSI.sh

--- CheckiSCSI.sh
+++ CheckiSCSI.sh
@@ -1,29 +1,57 @@
-#!/bin/bash  
+#!/bin/bash
+# check to ensure you are running as SUDO
+confini="/etc/tab/conf/default.ini"
+IPOK="no"
+
+get_settings () {
+  echo "Loading settings ..."
+  source $confini
+}
+
+save_settings () {
+  echo "Saving settings ..."
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" scriptver \"$scriptver\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" veeamxfs \"$veeamxfs\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" webmin \"$webmin\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" docker \"$docker\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" vupw \"$vupw\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" devnm \"$devnm\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" nasip \"$nasip\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" host \"$host\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" uuid \"$uuid\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" initiator \"$initiator\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" tapw \"$tapw\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" lturl \"$lturl\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" serverip \"$serverip\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" iscsifail \"$iscsifail\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" rebooted \"$rebooted\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" setup_cron \"$setup_cron\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" update_os \"$update_os\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" install_webmin \"$install_webmin\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" install_docker \"$install_docker\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" set_ip \"$set_ip\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" tabadmin_pw \"$tabadmin_pw\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" set_nasip \"$set_nasip\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" set_initiator \"$set_initiator\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" iscsi_conf \"$iscsi_conf\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" set_uuid \"$set_uuid\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" veeam_user \"$veeam_user\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" veeam_perms \"$veeam_perms\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" lt_installed \"$lt_installed\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" fstab_updated \"$fstab_updated\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" iscsi_edited \"$iscsi_edited\"
+  crudini --ini-options=nospace --set /etc/tab/conf/default.ini "" partitioned \"$partitioned\"  
+}
+
 if [[ "$HOSTNAME" == *"veeam"* ]]; then
   echo "Veeam server moving ahead."
 else
   exit
 fi
-if [ -d /mnt/veeamrepo/backups/ ]; then
-  echo "iSCSI OK"
-  if [ -f /etc/tab/scripts/iscsi.fail ]; then
-    echo "Removing old fail file..."
-    echo "$(date)" >> /etc/tab/logs/checkiscsi.log
-    echo "- iSCSI reconnected" >> /etc/tab/logs/checkiscsi.log
-    rm -f /etc/tab/scripts/iscsi.fail
-  fi
-else
-  if [ -f /etc/tab/scripts/iscsi.fail ]; then
-    echo "2nd fail, rebooting..."
-    echo "Removing old fail file..."
-    rm -f /etc/tab/scripts/iscsi.fail
-    echo "$(date)" >> /etc/tab/logs/checkiscsi.log
-    echo "- 2nd fail, rebooting" >> /etc/tab/logs/checkiscsi.log
-    shutdown -r now
-  else  
-    echo "iSCSI fail - dropping file..."
-    touch /etc/tab/scripts/iscsi.fail
-    echo "$(date)" >> /etc/tab/logs/checkiscsi.log
-    echo "- 1st fail, dropping file" >> /etc/tab/logs/checkiscsi.log
-  fi
-fi
+echo "========================================" >> /etc/tab/logs/checkiscsi.log
+echo "$(date)" >> /etc/tab/logs/checkiscsi.log
+echo "----------------------------------------" >> /etc/tab/logs/checkiscsi.log
+echo "- Testing iSCSI connection at the device level" >> /etc/tab/logs/checkiscsi.log
+lsscsi -t | grep %dev
+
+
