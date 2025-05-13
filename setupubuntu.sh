9c49ce769c4ba403f471373b89b2d9eb985020e2
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-05-13 12:16:57 -0400

    Create setupubuntu.sh

---
+++ setupubuntu.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# this will be a new revision; an all in one as it were
+# essential concept - all scripts now in /etc/tab/scrips; logs in /etc/tab/logs; config in /etc/tab/conf
+
+# location of config file
+confini="/etc/tab/conf/default.ini"
+
+# variables needed for various parts of the script but we do not need to save their data
+IPOK="no"
+iscsi_logged_in="no"
+
+# --------------------------------------------------[ procedure to load the config file
+get_settings () {
+  echo "Loading settings ..."
+  source $confini
+}
+
+# --------------------------------------------------[ procedure to save the config file
+save_settings () {
+  echo "Saving settings ..."
+  crudini --ini-options=nospace --set $confini "" scriptver \"$scriptver\"
+  crudini --ini-options=nospace --set $confini "" veeamxfs \"$veeamxfs\"
+  crudini --ini-options=nospace --set $confini "" webmin \"$webmin\"
+  crudini --ini-options=nospace --set $confini "" docker \"$docker\"
+  crudini --ini-options=nospace --set $confini "" vupw \"$vupw\"
+  crudini --ini-options=nospace --set $confini "" devnm \"$devnm\"
+  crudini --ini-options=nospace --set $confini "" nasip \"$nasip\"
+  crudini --ini-options=nospace --set $confini "" host \"$host\"
+  crudini --ini-options=nospace --set $confini "" uuid \"$uuid\"
+  crudini --ini-options=nospace --set $confini "" initiator \"$initiator\"
+  crudini --ini-options=nospace --set $confini "" mountpoint \"$mountpoint\"
+  crudini --ini-options=nospace --set $confini "" tapw \"$tapw\"
+  crudini --ini-options=nospace --set $confini "" lturl \"$lturl\"
+  crudini --ini-options=nospace --set $confini "" serverip \"$serverip\"
+  crudini --ini-options=nospace --set $confini "" iscsifail \"$iscsifail\"
+  crudini --ini-options=nospace --set $confini "" rebooted \"$rebooted\"
+  crudini --ini-options=nospace --set $confini "" setup_cron \"$setup_cron\"
+  crudini --ini-options=nospace --set $confini "" update_os \"$update_os\"
+  crudini --ini-options=nospace --set $confini "" install_webmin \"$install_webmin\"
+  crudini --ini-options=nospace --set $confini "" install_docker \"$install_docker\"
+  crudini --ini-options=nospace --set $confini "" set_ip \"$set_ip\"
+  crudini --ini-options=nospace --set $confini "" tabadmin_pw \"$tabadmin_pw\"
+  crudini --ini-options=nospace --set $confini "" set_nasip \"$set_nasip\"
+  crudini --ini-options=nospace --set $confini "" set_initiator \"$set_initiator\"
+  crudini --ini-options=nospace --set $confini "" iscsi_conf \"$iscsi_conf\"
+  crudini --ini-options=nospace --set $confini "" set_uuid \"$set_uuid\"
+  crudini --ini-options=nospace --set $confini "" veeam_user \"$veeam_user\"
+  crudini --ini-options=nospace --set $confini "" veeam_perms \"$veeam_perms\"
+  crudini --ini-options=nospace --set $confini "" lt_installed \"$lt_installed\"
+  crudini --ini-options=nospace --set $confini "" fstab_updated \"$fstab_updated\"
+  crudini --ini-options=nospace --set $confini "" iscsi_edited \"$iscsi_edited\"
+  crudini --ini-options=nospace --set $confini "" partitioned \"$partitioned\"  
+}
+
+# --------------------------------------------------[ procedure to wait for keystroke
+keystroke () {
+  echo
+  echo "Press any key to continue ..."
+  read -rsn1
+}
