c84137735e38ef39d015a0f2c6dc436051ca7d96
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-05-14 09:50:23 -0400

    Update setupubuntu.sh

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -340,6 +340,25 @@ update_os () {
   fi
 }
 
+#--------------------------------------------------[ Procedure to install Docker
+install_docker () {
+  echo ========== Installing Docker ==========
+  apt-get update
+  apt-get install ca-certificates curl
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt-get update
+  apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+  docker="installed"
+  install_docker="done"
+  save_settings
+}
+
 #--------------------------------------------------[ Procedure to install Webmin
 install_webmin () {
   echo "- Beginning install"
