f7a9c849d19939b73e6bd25f58617bb4b0ad3c17
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-05-15 15:50:23 -0400

    Update Healing.sh

--- Healing.sh
+++ Healing.sh
@@ -1,1 +1,19 @@
 
+heal_iscsi_files () {
+             umount $mountpoint2 &>> /etc/tab/logs/xfs_repair.log
+             if [ $? != 0 ] ; then
+               echo "- Dismount did not work, skipping the health test, see /etc/tab/logs/xfs_repair.log for details" >> /etc/tab/logs/checkiscsi.log
+             else
+               echo "- running xfs_repair on $devnm" >> /etc/tab/logs/xfs_repair.log
+               xfs_repair $devnm &>> /etc/tab/logs/xfs_repair.log
+               if [ $? != 0 ] ; then
+                 echo "- repair was not able to run, skipping, see /etc/tab/logs/xfs_repair.log for details" >> /etc/tab/logs/checkiscsi.log
+               else
+                 echo "- remounting file system" >> /etc/tab/logs/xfs_repair.log
+                 mount -a &>> /etc/tab/logs/xfs_repair.log
+                if [ $? != 0 ] ; then
+                  echo "- remounting failed! see /etc/tab/logs/xfs_repair.log for details" >> /etc/tab/logs/checkiscsi.log
+                else
+                  echo "- Repair done, see /etc/tab/logs/xfs_repair.log for details" >> /etc/tab/logs/checkiscsi.log
+
+}
